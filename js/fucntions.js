a3ae5d87d3d4e38154869ad2d302b9295b168a66
rirefat <rirefat.official@gmail.com>
2022-02-11 04:26:19 +0600

    Login Validation Done

---
+++ js/fucntions.js
@@ -0,0 +1,28 @@
+// Function for user login 
+function userValidation(){
+    const uEmail = document.getElementById('uEmail');
+    const emailValue = uEmail.value;
+    const uPass = document.getElementById('uPass');
+    const passValue = uPass.value;
+    uEmail.value = '';
+    uPass.value = '';
+    console.log(emailValue);
+    console.log(passValue);
+    if(emailValue == user_email && passValue == user_pass){
+        window.location.href = 'calculation-system.html';
+        console.log('valid');
+    }
+    else{
+        document.body.style.backgroundColor='#ffe4e6';
+        const alertText = document.getElementById('wrongInfo');
+        console.log(alertText);
+        alertText.innerText ="You've entered wrong email or password. Try again with correct user information.";
+    }
+}
+
+// Function for user logout
+// document.getElementById('logout-btn').addEventListener('click', logout);
+
+// function logout(){
+//     window.location.href('index.html');
+// }
